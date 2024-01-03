62ef69226204a5a1b63b10c43e9d77fbe5d85157
Kest <meowtss@protonmail.com>
2024-01-03 13:15:00 -0500

    Updated files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import BlogPage from './components/BlogPage.vue';
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    BlogPage
-  }
-});
-</script>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous"
+  />
+  <!--Card Start-->
+  <div class="row p-4">
+    <div class="col-sm-3 mb-3 mb-sm-4">
+      <div class="card" style="width: 18rem">
+        <img
+          src="../assets/logo.png"
+          class="card-img-top img-thumbnail"
+          alt="../assets/logo.png"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Building this website</h5>
+          <p class="card-text">
+            This website is documenting my current progress in constructing and
+            maintaining this website.
+          </p>
+          <router-link to="/about">Blog</router-link>
+        </div>
+      </div>
+    </div>
+    <!--Card end-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
