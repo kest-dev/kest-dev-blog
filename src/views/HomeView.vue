02a91d3ee8469cc94a9be6e12d051b40a403ead3
Kest <meowtss@protonmail.com>
2024-01-03 13:52:30 -0500

    Updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,6 @@
 <template>
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-    crossorigin="anonymous"
-  />
   <!--Card Start-->
-  <div class="row p-4">
+  <div class="row p-4" data-bs-theme="dark">
     <div class="col-sm-3 mb-3 mb-sm-4">
       <div class="card" style="width: 18rem">
         <img
@@ -15,12 +9,14 @@
           alt="../assets/blog1.png"
         />
         <div class="card-body">
-          <h5 class="card-title">Building this website</h5>
+          <h5 class="card-title">
+            <router-link to="/blog1">Building this website</router-link>
+          </h5>
+          <p class="card-text">Last Modified: 1/3/2024</p>
           <p class="card-text">
             This website is documenting my current progress in constructing and
             maintaining this website.
           </p>
-          <router-link to="/about">Blog</router-link>
         </div>
       </div>
     </div>
