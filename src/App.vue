33c32aeee8fa6e230ce1b4f076ff000c830545c7
Kest <meowtss@protonmail.com>
2024-01-03 10:31:10 -0500

    Updated page and created a blog page

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BlogPage from './components/BlogPage.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BlogPage
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
