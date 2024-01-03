51a40b78589913b7fcbb37c78f8670fd9c2f80ed
Kest <meowtss@protonmail.com>
2024-01-03 14:46:42 -0500

    foo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+console.log("foo");
+createApp(App).use(router).mount("#app");
